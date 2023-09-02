e9e164b8f633992898acdc9d3ff88e7f1a7e6683
Alamgir <agir97@gmail.com>
2023-09-02 14:32:32 +0600

    Created array by view amounts

--- js/script.js
+++ js/script.js
@@ -1,36 +1,36 @@
 const loadCategory = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
-    const data = await res.json();
-    categories = data.data
-    //console.log(categories);
-    displayCategory(categories);
+  const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+  const data = await res.json();
+  categories = data.data
+  //console.log(categories);
+  displayCategory(categories);
 }
 
 const displayCategory = categories => {
-    const categoryContainer = document.getElementById('category-container');
+  const categoryContainer = document.getElementById('category-container');
 //console.log(categoryContainer);
 
 categories.forEach(category => {
-    console.log(category);
-    const categoryButton = document.createElement('div');    
-    categoryButton.classList = "bg-red-200 p-2 m-4 active:bg-red-400 hover:bg-red-600 visited:bg-red-800";    
-    categoryButton.innerHTML = `<button onclick="displayCardContainer('${category.category_id}')"> ${category.category}</button>`; 
-    console.log(category.category_name);   
-    categoryContainer.appendChild(categoryButton);
-    //console.log(categoryButton);
-    
-    
+  console.log(category);
+  const categoryButton = document.createElement('div');    
+  categoryButton.classList = "bg-red-200 p-2 m-4 active:bg-red-400 hover:bg-red-600 visited:bg-red-800";    
+  categoryButton.innerHTML = `<button onclick="displayCardContainer('${category.category_id}')"> ${category.category}</button>`; 
+  console.log(category.category_name);   
+  categoryContainer.appendChild(categoryButton);
+  //console.log(categoryButton);
+  
+  
 });
 
 };
 
 const displayCardContainer = async (categoryId) => {
-    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
+  const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
 
-    const data = await res.json();
-    console.log(data);
-    const categories = data.data;
-    displayCard(categories);
+  const data = await res.json();
+  console.log(data);
+  const categories = data.data;
+  displayCard(categories);
 }
 
 
@@ -40,72 +40,79 @@ const displayCardContainer = async (categoryId) => {
 
 
 
-    const displayCard = categories => {
-      //console.log(categories.length);
-   
+  const displayCard = categories => {
+    //console.log(categories.length);
+ 
 
-    const cardContainer = document.getElementById('card-container');
-    //console.log('cardContainer');
-    cardContainer.innerHTML = "";
+  const cardContainer = document.getElementById('card-container');
+  //console.log('cardContainer');
+  cardContainer.innerHTML = "";
 
-    if(categories.length===0){
-      
-      const card = document.createElement('div');    
-        card.classList = "mt-auto mx-auto flex";         
-        card.innerHTML = `         
-        <img class="w-12 h-12 rounded-full" src="./86-PHero-tube-main/Icon.png" />       
-        <br>
-        <h2 class="card-title">Opps!! Sorry, There is no content here</h2> 
-      `      
-       cardContainer.appendChild(card)
-     }
-
-
-
-
-
-
-
-    categories.forEach(elem => {
-
-     
-        
-        //console.log(elem.authors[0].profile_name);
-        //console.log(category);
-        const card = document.createElement('div');    
-        card.classList = "bg-red-200 p-2 m-4 active:bg-red-400 hover:bg-red-600 visited:bg-red-800";         
-        card.innerHTML = `
-        <figure><img src="${elem.thumbnail}" /></figure>
-        
-        
-        <div class="p-2 flex bg-base-200" >
+  if(categories.length===0){
     
-        
-        <div class="avatar">
-      <div class="w-12 h-12 rounded-full">
-        <img src="${elem.authors[0].profile_picture}" />       
-      </div>
-    </div>
-    
-    
-        <div class="card-body">
-          <h2 class="card-title">${elem.title}</h2>
-          
-          <div class="card-actions">
-            <p>${elem.authors[0].profile_name}</p><input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />    
-          </div>
-    
-          <p>${elem.others.views}</p>
-        </div>
-    
-    
-        </div>
-     `
+    const card = document.createElement('div');    
+      card.classList = "mt-auto mx-auto flex";         
+      card.innerHTML = `         
+      <img class="w-12 h-12 rounded-full" src="./86-PHero-tube-main/Icon.png" />       
+      <br>
+      <h2 class="card-title">Opps!! Sorry, There is no content here</h2> 
+    `      
      cardContainer.appendChild(card)
-    })
-    };
+   }
+
+
+
+
+let elements = []
+//reverse element is not working properly
+let reverseElements = elements.sort(function(a, b) {
+  return b - a})
+console.log(elements);
+console.log(reverseElements);
+
+
+  categories.forEach(elem => {
+
+   const element =elem.others.views.slice(0,-1)
+   console.log(element)
+   elements.push(element)
+      
+      //console.log(elem.authors[0].profile_name);
+      //console.log(category);
+      const card = document.createElement('div');    
+      card.classList = "bg-red-200 p-2 m-4 active:bg-red-400 hover:bg-red-600 visited:bg-red-800";         
+      card.innerHTML = `
+      <figure><img src="${elem.thumbnail}" /></figure>
+      
+      
+      <div class="p-2 flex bg-base-200" >
   
-   
+      
+      <div class="avatar">
+    <div class="w-12 h-12 rounded-full">
+      <img src="${elem.authors[0].profile_picture}" />       
+    </div>
+  </div>
+  
+  
+      <div class="card-body">
+        <h2 class="card-title">${elem.title}</h2>
+        
+        <div class="card-actions">
+          <p>${elem.authors[0].profile_name}</p><input type="radio" name="rating-2" class="mask mask-star-2 bg-orange-400" />    
+        </div>
+  
+        <p>${elem.others.views}</p>
+      </div>
+  
+  
+      </div>
+   `
+   cardContainer.appendChild(card)
+  })
+  };
+
+ 
 
 
 //displayCardContainer(categoryId) 
